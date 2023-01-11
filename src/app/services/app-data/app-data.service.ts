6cf0a15d551febb44a7f81c19640f5686937bfcc
GetsemaniSal <getsemanisalazar7@gmail.com>
2023-01-11 13:13:48 -0600

    Update app-data.service.ts

--- src/app/services/app-data/app-data.service.ts
+++ src/app/services/app-data/app-data.service.ts
@@ -3,13 +3,17 @@ import { environment } from 'src/environments/environment';
 import {HttpClient} from '@angular/common/http'
 @Injectable()
 
-
 export class AppDataService {
-
   private usuarioEndPoint: string ='https://auth.sev.gob.mx/basic/centro-trabajo';
 
 
-  // constructor(pdb: ProblemDetailsBuilder, private http: HttpClient) {
-  //   super(pdb, environment.production);
-  // }
+  constructor(
+    public http: HttpClient
+) {
+}
+
+public Login(login: any) {
+    return this.http.post(this.usuarioEndPoint,login);
+    
+}
 }
