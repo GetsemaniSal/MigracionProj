446afe9418530e90b00f58681fa0f5652e217d2c
GetsemaniSal <getsemanisalazar7@gmail.com>
2023-01-30 12:15:29 -0600

    Ni idea de que estoy haciendo
    Supuestamente creé un arhcivo json para rederigir

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { StatusBar } from '@ionic-native/status-bar';
-import {SplashScreen} from '@ionic-native/splash-screen';
+import {SplashScreen} from '@ionic-core/splashScreen';
 import { HomePage } from './home/home.page';
  @Component({
    selector: 'app-root',
